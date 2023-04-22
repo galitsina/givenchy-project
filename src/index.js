982ce6476848aac48f3d97d46dfdd34c3f9ecd0f
Daniella Galitsyna <galitsina@bstec.ru>
2023-04-22 20:19:16 +0200

    set up environment

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './pages/index.css';
+
+import headerImage from './images/header.jpg';
+import kendallImage from './images/kendall.jpg';
+
+const images = [
+  // меняем исходные пути на переменные
+  { name: 'Header', image: headerImage },
+  { name: 'Kendall', link: kendallImage },
+];
