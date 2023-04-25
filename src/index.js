6694ff8f7f0f3b2e47e717ae8d18d795885f0320
Daniella Galitsyna <galitsina@bstec.ru>
2023-04-25 14:42:32 +0200

    header

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import './pages/index.scss';
 
 const menu = document.querySelector('.menu__body');
-const menuButton = document.querySelector('.menu__icon');
+const menuButton = document.querySelector('.menu__hamburger');
+const body = document.body;
 
 if (menu && menuButton) {
   menuButton.addEventListener('click', () => {
     menu.classList.toggle('menu__body_active');
     menuButton.classList.toggle('menu__hamburger_active');
+    body.classList.toggle('page_lock');
+  })
+
+  menu.querySelectorAll('.menu__link').forEach(link => {
+    link.addEventListener('click', () => {
+      menu.classList.remove('menu__body_active');
+      menuButton.classList.remove('menu__hamburger_active');
+      body.classList.remove('page_lock');
+    })
   })
 }
